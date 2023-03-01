4e6790dcb9edc4d663298e909ad0fdcff136218d
letr2303 <letr2303@usherbrooke.ca>
2023-03-01 16:42:24 -0500

    add eta assistance from dustingTable for testing
    just test if we can use that but with multiple surfaces

--- Assets/Scripts/Scenarios/BehaviorTrees/WateringThePlants.cs
+++ Assets/Scripts/Scenarios/BehaviorTrees/WateringThePlants.cs
@@ -29,8 +29,16 @@ namespace MATCH
                 string ConditionOnePlantWatered = "OnePlantWatered";
                 string ConditionAllPlantsWatered = "AllPlantsWatered";
 
+
+                string InferenceFarFromRag = "FarFromRag";
+                float InferenceFarFromRagDistance = 4.0f;
+                string InferenceDidNotStartWatering = "DidNotStartWatering";
+                string InferenceInterruptWatering = "InterruptedWatering";
+
                 // Interaction surface
                 Assistances.InteractionSurface InteractionSurfaceDialogs;
+                Assistances.InteractionSurface InteractionRag;
+
                 Assistances.InteractionSurface InteractionSink;
                 Assistances.InteractionSurface InteractionPlant1;
                 Assistances.InteractionSurface InteractionPlant2;
@@ -64,6 +72,8 @@ namespace MATCH
 
                 protected override Root InitializeBehaviorTree()
                 {
+                    // Making the conditions update.
+                    // SEE EXCEL SHEET TO GENERATE THE CODE BELOW
                     AddCondition(ConditionBeginning, false);
                     AddCondition(ConditionBottleFilled, false);
                     AddCondition(ConditionHelpNeeded, false);
@@ -72,21 +82,43 @@ namespace MATCH
                     AddCondition(ConditionOnePlantWatered, false);
                     AddCondition(ConditionAllPlantsWatered, false);
 
+
                     int nbConditions = GetNumberOfConditions();
 
-                    AddConditionsUpdate(ConditionBeginning, new bool[] { true, false, false, false, false, false, false });
-                    AddConditionsUpdate(ConditionBottleFilled, new bool[] { false, true, false, false, false, false, false });
-                    AddConditionsUpdate(ConditionHelpNeeded, new bool[] { false, false, true, false, false, false, false });
-                    AddConditionsUpdate(ConditionHelpRequested, new bool[] { false, false, false, true, false, false, false });
+                    AddConditionsUpdate(ConditionBeginning, new bool[]          { true, false, false, false, false, false, false });
+                    AddConditionsUpdate(ConditionBottleFilled, new bool[]       { false, true, false, false, false, false, false });
+                    AddConditionsUpdate(ConditionHelpNeeded, new bool[]         { false, false, true, false, false, false, false });
+                    AddConditionsUpdate(ConditionHelpRequested, new bool[]      { false, false, false, true, false, false, false });
                     AddConditionsUpdate(ConditionHelpRequestedAgain, new bool[] { false, false, false, false, true, false, false });
-                    AddConditionsUpdate(ConditionOnePlantWatered, new bool[] { false, false, false, false, false, true, false });
-                    AddConditionsUpdate(ConditionAllPlantsWatered, new bool[] { false, false, false, false, false, false, true });
+                    AddConditionsUpdate(ConditionOnePlantWatered, new bool[]    { false, false, false, false, false, true, false });
+                    AddConditionsUpdate(ConditionAllPlantsWatered, new bool[]   { false, false, false, false, false, false, true });
+
+
+                    // End of code generation using the EXCEL file
 
                     UpdateConditionWithMatrix(ConditionBeginning);
 
+                    //Defining the BT
+                    //Selector srBottleNotTaken = new Selector(
+                    //    new BlackboardCondition(ConditionBeginning, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP7())
+                    //    );
+
+                    //Selector srPlantsNotWatered = new Selector(
+                    //    new BlackboardCondition(ConditionBeginning, Operator.IS_EQUAL, false, Stops.IMMEDIATE_RESTART, srBottleNotTaken),
+                    //    new BlackboardCondition(ConditionHelpNeeded, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP4()),
+                    //    new BlackboardCondition(ConditionOnePlantWatered, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP2()),
+                    //    new BlackboardCondition(ConditionTwoPlantWatered, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP2()),
+                    //    new BlackboardCondition(ConditionThreePlantWatered, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP2()),
+                    //    new BlackboardCondition(ConditionHelpRequestedAgain, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP3()),
+                    //    new WaitUntilStopped()
+                    //    );
+
+                    //Selector srBegin = new Selector(
+                    //    new BlackboardCondition(ConditionAllPlantsWatered, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP1()));
+
                     Selector srBegin = new Selector(
                         new BlackboardCondition(ConditionBeginning, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP7()),
-                        new BlackboardCondition(ConditionBottleFilled, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP5()),
+                        new BlackboardCondition(ConditionBottleFilled, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceEta()),
                         new BlackboardCondition(ConditionHelpNeeded, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP4()),
                         new BlackboardCondition(ConditionHelpRequested, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP6()),
                         new BlackboardCondition(ConditionHelpRequestedAgain, Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, AssistanceBTWP3()),
@@ -148,9 +180,9 @@ namespace MATCH
                     InteractionPlant1 = Assistances.Factory.Instance.CreateInteractionSurface("Practice-Plant1", AdminMenu.Panels.Right, new Vector3(0.3f, 0.5f, 0.3f),
                         new Vector3(-2.309f, 0.263f, 2.031f), Utilities.Materials.Colors.GreenGlowing, false, true, Utilities.Utility.GetEventHandlerEmpty(), true, transform);
                     InteractionPlant2 = Assistances.Factory.Instance.CreateInteractionSurface("Practice-Plant2", AdminMenu.Panels.Right, new Vector3(0.3f, 0.5f, 0.3f),
-                        new Vector3(0f, 0f, 0f), Utilities.Materials.Colors.GreenGlowing, false, true, Utilities.Utility.GetEventHandlerEmpty(), true, transform);
+                        new Vector3(2f, 0f, 0f), Utilities.Materials.Colors.GreenGlowing, false, true, Utilities.Utility.GetEventHandlerEmpty(), true, transform);
                     InteractionPlant3 = Assistances.Factory.Instance.CreateInteractionSurface("Practice-Plant3", AdminMenu.Panels.Right, new Vector3(0.3f, 0.5f, 0.3f),
-                         new Vector3(0f, 0f, 0f), Utilities.Materials.Colors.GreenGlowing, false, true, Utilities.Utility.GetEventHandlerEmpty(), true, transform);
+                         new Vector3(-7f, 0f, 0f), Utilities.Materials.Colors.GreenGlowing, false, true, Utilities.Utility.GetEventHandlerEmpty(), true, transform);
 
                     
                     InteractionSink.EventInteractionSurfaceTableTouched += CallbackInteractionSurfaceSinkTouched;
@@ -163,14 +195,68 @@ namespace MATCH
                 //Is it 7pm?
                 Sequence AssistanceBTWP7()
                 {
+                    Assistances.GradationVisual.GradationVisual alpha1 = Assistances.GradationVisual.Factory.Instance.CreateDialog2WithButtons("Practice-Alpha-1", "",
+                        "Qu'est-ce qu'il est conseilé de faire à la fin de la journée lorsqu'il ne fait moins chaud?", "Commencer !", delegate (System.Object o, EventArgs e)
+                    {
+                        UpdateConditionWithMatrix(ConditionBottleFilled);
+                    }, Assistances.Buttons.Button.ButtonType.ClosingButton, InteractionSurfaceDialogs.transform);
+
+
+                    Assistances.AssistanceGradationExplicit alpha = MATCH.Assistances.Factory.Instance.CreateAssistanceGradationExplicit("ArroserLesPlantes-Alpha");
+                    alpha.transform.parent = transform;
+
+                    alpha.AddAssistance(alpha1, Assistances.Buttons.Button.ButtonType.ClosingButton, null);
+
+                    AssistancesWatering.Add(alpha, false);
+
+                    alpha.Init();
+
                     Sequence temp = new Sequence(
                         new NPBehave.Action(() => {
-                        
-         
+                            ShowAssistanceHideOthers(alpha);
                             UpdateTextAssistancesDebugWindow("Is it 7pm?");
                             MATCH.Utilities.Logger.Instance.Log(this.GetId(), MethodBase.GetCurrentMethod().ReflectedType.Name, MethodBase.GetCurrentMethod().Name, "BTWP7");
                         }),
                         new WaitUntilStopped()
+                        );
+
+                    return temp;
+                }
+
+                Sequence AssistanceEta()
+                {
+                    Assistances.AssistanceGradationExplicit eta = MATCH.Assistances.Factory.Instance.CreateAssistanceGradationExplicit("Dusting - Eta");
+                    eta.transform.parent = transform;
+                    //eta.InfManager = InferenceManager;
+
+                    Assistances.GradationVisual.GradationVisual eta1 = Assistances.GradationVisual.Factory.Instance.CreateSurfaceToProcess("DustingTable-Eta-1", delegate (System.Object o, EventArgs e)
+                    {
+                        ShowAssistanceHideOthers(eta);
+                        UpdateConditionWithMatrix(ConditionOnePlantWatered);
+                    }, delegate (System.Object o, EventArgs e)
+                    {
+                        UpdateConditionWithMatrix(ConditionAllPlantsWatered);
+                    }, InteractionPlant1, InteractionPlant1.transform);
+                    //eta1
+
+
+                    /*Assistances.GradationVisual.GradationVisual beta2 = Assistances.GradationVisual.Factory.Instance.CreateDialogTwoButtons("DustingTable-Gamma-2", "", "Il y a une activité à faire ici", "Je sais!", Utilities.Utility.GetEventHandlerEmpty(), Assistances.Buttons.Button.ButtonType.Yes, "Je ne sais pas", Utilities.Utility.GetEventHandlerEmpty(), Assistances.Buttons.Button.ButtonType.No, InteractionSurfaceTable.transform);*/
+
+                    eta.AddAssistance(eta1, Assistances.Buttons.Button.ButtonType.Undefined, null);
+
+                    AssistancesWatering.Add(eta, false);
+
+                    eta.Init();
+
+                    Sequence temp = new Sequence(
+                        new NPBehave.Action(() =>
+                        {
+                            ShowAssistanceHideOthers(eta);
+                            OnChallengeStart();
+
+                            UpdateTextAssistancesDebugWindow("Eta");
+                            MATCH.Utilities.Logger.Instance.Log(this.GetId(), MethodBase.GetCurrentMethod().ReflectedType.Name, MethodBase.GetCurrentMethod().Name, "Eta");
+                        })
                         );
 
                     return temp;
@@ -261,12 +347,26 @@ namespace MATCH
                 //Are all plants watered?
                 Sequence AssistanceBTWP1()
                 {
+                    Assistances.GradationVisual.GradationVisual gradationVisual_BTWP1 = Assistances.GradationVisual.Factory.Instance.CreateDialog2WithButtons("Watering-BTWP-1", "",
+                        "Vous avez terminé l'activité! Félicitations!", "Terminer", Utilities.Utility.GetEventHandlerEmpty(), Assistances.Buttons.Button.ButtonType.ClosingButton, InteractionSurfaceDialogs.transform);
+
+
+                    Assistances.AssistanceGradationExplicit gradationExplicit_BTWP1 = MATCH.Assistances.Factory.Instance.CreateAssistanceGradationExplicit("Watering-BTWP1");
+                    gradationExplicit_BTWP1.transform.parent = transform;
+
+                    gradationExplicit_BTWP1.AddAssistance(gradationVisual_BTWP1, Assistances.Buttons.Button.ButtonType.ClosingButton, null);
+
+                    AssistancesWatering.Add(gradationExplicit_BTWP1, false);
+
+                    gradationExplicit_BTWP1.Init();
+
                     Sequence temp = new Sequence(
                         new NPBehave.Action(() => {
-                            //ShowAssistanceHideOthers(alpha);
-         
+                            ShowAssistanceHideOthers(gradationExplicit_BTWP1);
+                            InferenceManager.UnregisterAllInferences();
                             UpdateTextAssistancesDebugWindow("BTWP1");
                             MATCH.Utilities.Logger.Instance.Log(this.GetId(), MethodBase.GetCurrentMethod().ReflectedType.Name, MethodBase.GetCurrentMethod().Name, "BTWP1");
+                            OnChallengeSuccess();
                         }),
                         new WaitUntilStopped()
                         );
@@ -305,6 +405,24 @@ namespace MATCH
                         }
                     }
                 }
+
+                //void CallbackInteractionSurfaceRagTouched(System.Object o, EventArgs e)
+                //{
+                //    InferenceManager.UnregisterInference(InferenceFarFromRag);
+                //    UpdateConditionWithMatrix(ConditionBeginning);
+                //}
+
+                //void RegisterInferenceFarFromRag()
+                //{
+                //    DebugMessagesManager.Instance.displayMessage(MethodBase.GetCurrentMethod().ReflectedType.Name, MethodBase.GetCurrentMethod().Name, DebugMessagesManager.MessageLevel.Info, "Registering inference distance from rag with distance value: " + InferenceFarFromRagDistance);
+
+                //    InferenceManager.UnregisterInference(InferenceFarFromRag);
+
+                //    MATCH.Inferences.Factory.Instance.CreateDistanceLeavingInferenceOneShot(InferenceManager, InferenceFarFromRag, delegate (System.Object oo, EventArgs ee)
+                //    {
+                //        UpdateConditionWithMatrix(ConditionRagNotTakenButHelpReceived);
+                //    }, InteractionRag.gameObject, InferenceFarFromRagDistance);
+                //}
             }
 
         }
